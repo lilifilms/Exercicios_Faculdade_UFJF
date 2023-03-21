e9e509ebb06823bd31c6f0461a4a01b7004a29dd
maquina-lilifilms <elisavilelauzumaki@gmail.com>
2023-03-20 22:44:39 -0300

    feature: ex calcular volume facu Ufjf

---
+++ exercicio1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float pi = 3.14159;
+
+    int height = 12;
+    int raio = 3;
+    //calc volume
+    int volume = (pi * (raio * raio) * height);
+
+    //Exibir o valor de volume na tela
+    cout <<  volume;
+}
