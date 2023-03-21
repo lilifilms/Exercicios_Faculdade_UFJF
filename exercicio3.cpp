e89774d5c83d8d247630e529f86481e2d4bff00a
maquina-lilifilms <elisavilelauzumaki@gmail.com>
2023-03-20 23:21:48 -0300

    feature: ex3 calc dobro,triplo,dobro facul UFJF

---
+++ exercicio3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   float valorX = 6;
+   float calcDobro = (valorX * 2);
+   float calcTriplo = (valorX * 3);
+   float calcQuadrado = (valorX * valorX);
+
+   cout << "valor de X: " << valorX << "\n" << "dobro: " << calcDobro << "\n" << "triplo: " << calcTriplo << "\n" << "quadrado: " << calcQuadrado;
+   
+}
